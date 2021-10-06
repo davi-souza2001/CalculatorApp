c579de6834c840b5ced02ed3dd958b9afd3716eb
davi-souza2001 <davisouza2001dv@gmail.com>
2021-10-06 08:15:13 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+import First from "./components/First";
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <First/>
+    </SafeAreaView>
+  )
+}
+
